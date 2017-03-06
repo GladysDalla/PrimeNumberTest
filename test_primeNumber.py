83b80e13b7f25a38031e75889b9aef381998dfc4
qty05 <gldsdalla@gmail.com>
2017-03-07 01:22:33 +0300

    second test

--- test_primeNumber.py
+++ test_primeNumber.py
@@ -4,10 +4,12 @@ from solution_prime import is_prime_number
 class TestPrimeNumbers(unittest.TestCase):
 	"""tests for 'solution_prime.py'."""
 
-	def test_input_is_true(self):
+	def test_method_is_true(self):
 		"""tests if the output ranging from 2 to 10 are prime numbers"""
 		self.assertTrue(is_prime_number(2, 10))
-
+	def test_input_is_not_string(self):
+		"""tests if string is passed"""
+		self.assertRaises(ValueError,is_prime_number('this', 10))
 if __name__ == '__main__':
     unittest.main()
 
