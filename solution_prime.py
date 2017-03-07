1a903c6368741a37f992a42ad36fbf50d10a091d
qty05 <gldsdalla@gmail.com>
2017-03-07 09:50:43 +0300

    new changes

--- solution_prime.py
+++ solution_prime.py
@@ -1,7 +1,7 @@
 def is_prime_number():
 	"""finds prime numbers  between range 0 and n"""
 	
-	for number in range (0,n):
+	for number in range (0,math.sqrt(n)):
 		if number > 1:
 			for i in range (2,number):
 				if (num % i) == 0:
